7621eaaf57f31c3be2b52cce1168a41b2865c775
Viktoryia_Kuushynava <Viktoryia_Kuushynava@epam.com>
2022-01-17 13:32:21 +0300

    commit

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-
-    $(".buildtable").click(loaddata);
-    function loaddata() {
-        var select = document.getElementById('valute');
-        var value = select.options[select.selectedIndex].value;
-        var result = document.querySelector('input').value;
-        $.getJSON("https://www.cbr-xml-daily.ru/daily_json.js", function (data) {
-            $('#output').html(result + " " + data.Valute[value].Name + "  -> " + data.Valute[value].Value.toFixed(1) * result + " Рублей");
-        })
-    };
+$(".buildtable").click(loaddata);
+function loaddata() {
+    var select = document.getElementById('valute');
+    var value = select.options[select.selectedIndex].value;
+    var result = document.querySelector('input').value;
+    $.getJSON("https://www.cbr-xml-daily.ru/daily_json.js", function (data) {
+        $('#output').html(result + " " + data.Valute[value].Name + "  -> " + data.Valute[value].Value.toFixed(1) * result + " Рублей");
+    })
+};
