2e8d5a49b0f589a4b6f380da2e94c389ac5dca33
Viktoryia_Kuushynava <Viktoryia_Kuushynava@epam.com>
2022-01-17 13:05:52 +0300

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    $(".buildtable").click(loaddata);
+    function loaddata() {
+        var select = document.getElementById('valute');
+        var value = select.options[select.selectedIndex].value;
+        var result = document.querySelector('input').value;
+        $.getJSON("https://www.cbr-xml-daily.ru/daily_json.js", function (data) {
+            $('#output').html(result + " " + data.Valute[value].Name + "  -> " + data.Valute[value].Value.toFixed(1) * result + " Рублей");
+        })
+    }
+});
